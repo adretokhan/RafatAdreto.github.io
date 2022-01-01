67ec21840647f31ed3f6b819638095e912063634
adretokhan <adreto.khan@gmail.com>
2021-12-31 19:39:55 -0800

    nothing actually

--- rml.js
+++ rml.js
@@ -21,3 +21,25 @@ function myFunction() {
 
 
 
+function show() {
+
+  
+  
+  var email= document.getElementById("exampleInputEmail1").value;
+  var name = document.getElementById("exampleInputPassword1").value;
+
+   firebase.database().ref('User/' + email).set({
+          
+          
+          email : email,
+          name : name
+        }, function(error) {
+          if (error) {
+            alert("Failed");
+          } else {
+              alert("DONE");
+            
+         
+          }
+        });
+} 
