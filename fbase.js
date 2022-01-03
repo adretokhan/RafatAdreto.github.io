a84cdf84c105b2c744b85d0a44e050d7a4bd192a
adretokhan <adreto.khan@gmail.com>
2022-01-02 19:59:20 -0800

    emni

--- fbase.js
+++ fbase.js
@@ -1,9 +1,11 @@
+
+
 function rafat() {
+  
 
 
-
-    var email = document.getElementById("exampleInputEmail1").value;
-    var name = document.getElementById("exampleInputPassword1").value;
+    var email = document.getElementById('email').value;
+    var name = document.getElementById('name').value;
   
     firebase.database().ref('User/' + email).set({
   
