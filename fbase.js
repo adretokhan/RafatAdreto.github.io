ffc91dc2208f5bc03c493cecdbe01df686fffb04
adretokhan <adreto.khan@gmail.com>
2022-01-09 19:42:19 -0800

    emni

--- fbase.js
+++ fbase.js
@@ -1,30 +1,29 @@
 
 
 function rafat() {
-  
-
-
-    var email = document.getElementById('email').value;
-    var name = document.getElementById('name').value;
-  
-    firebase.database().ref('User/' + name).set({
-  
-  
-      email: email,
-      name: name
-    }, function (error) {
-      if (error) {
-        alert("Failed");
-      } else {
-        alert("DONE");
-  
-  
-      }
-    });
-  } 
 
 
 
+  var email = document.getElementById('email').value;
+  var name = document.getElementById('name').value;
+
+  firebase.database().ref('User/' + name).set({
 
 
-  
+    email: email,
+    name: name
+  }, function (error) {
+    if (error) {
+      alert("Failed");
+    } else {
+      alert("DONE");
+
+
+    }
+  });
+}
+
+
+
+
+
