7fa9663045f967a38521db7f78a9c59ffbb49f8c
adretokhan <adreto.khan@gmail.com>
2022-01-13 22:23:43 -0800

    emni

--- fbase.js
+++ fbase.js
@@ -21,7 +21,7 @@ function rafat() {
 
     }
   });
-  retrieveinfos();
+  // retrieveinfos();
 }
 function retrieveinfos(){
   let ref=firebase.database().ref('User');
@@ -36,10 +36,11 @@ function gotdata(data){
     let email=info[infodata].email;
     
     let infores=document.querySelector('.inforesults');
-    infores.innerHTML += `<div>
+    infores.innerHTML += `<div><center>
       <p><strong>Name: </strong>${name} <br/>
       <a><strong>email: </strong>${email}</a><br/>
       </p>
+      </center>
     </div>`;
   }
 }
